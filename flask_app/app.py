2b58267e624a7f1c0ecea4d4a4b26c562e93c403
Saiful0044 <saifulislam00324@gmail.com>
2025-07-25 23:05:20 +0600

    add flask app testing code 3

--- flask_app/app.py
+++ flask_app/app.py
@@ -1,11 +1,81 @@
 from flask import Flask,render_template,request
-import preprocessing_utility as ppu
 import mlflow.pyfunc
 import pickle
 import pandas as pd
 from dotenv import load_dotenv
-import os
 import mlflow
+import numpy as np
+import pandas as pd
+import os
+import re
+import nltk
+import string
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
+
+nltk.download("wordnet")
+nltk.download("stopwords")
+
+
+def lemmatization(text):
+    """Lemmatize the text."""
+    lemmatizer = WordNetLemmatizer()
+    text = text.split()
+    text = [lemmatizer.lemmatize(word) for word in text]
+    return " ".join(text)
+
+
+def remove_stop_words(text):
+    """Remove stop words from the text."""
+    stop_words = set(stopwords.words("english"))
+    text = [word for word in str(text).split() if word not in stop_words]
+    return " ".join(text)
+
+
+def removing_numbers(text):
+    """Remove numbers from the text."""
+    text = "".join([char for char in text if not char.isdigit()])
+    return text
+
+
+def lower_case(text):
+    """Convert text to lower case."""
+    text = text.split()
+    text = [word.lower() for word in text]
+    return " ".join(text)
+
+
+def removing_punctuations(text):
+    """Remove punctuations from the text."""
+    text = re.sub("[%s]" % re.escape(string.punctuation), " ", text)
+    text = text.replace("؛", "")
+    text = re.sub("\s+", " ", text).strip()
+    return text
+
+
+def removing_urls(text):
+    """Remove URLs from the text."""
+    url_pattern = re.compile(r"https?://\S+|www\.\S+")
+    return url_pattern.sub(r"", text)
+
+
+def remove_small_sentences(df):
+    """Remove sentences with less than 3 words."""
+    for i in range(len(df)):
+        if len(df.text.iloc[i].split()) < 3:
+            df.text.iloc[i] = np.nan
+
+
+def normalize_text(text):
+    text = lower_case(text)
+    text = remove_stop_words(text)
+    text = removing_numbers(text)
+    text = removing_punctuations(text)
+    text = removing_urls(text)
+    text = lemmatization(text)
+
+    return text
+
 
 # model = mlflow.pyfunc.load_model("models:/MyRegisterModel@production")
 # vectorizer = pickle.load(open("models/vectorizer.pkl", "rb"))
@@ -58,7 +128,7 @@ def predict():
     text = request.form['text']
 
     # clean
-    text = ppu.normalize_text(text)
+    text = normalize_text(text)
 
     # bow
     features = vectorizer.transform([text])
