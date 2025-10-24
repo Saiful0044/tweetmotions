f9ecfcd32d2f5a7818f9d47e17086566c9aaaade
Saiful0044 <saifulislam00324@gmail.com>
2025-10-24 07:55:27 +0600

    change appspec.yml file

--- deploy/scripts/start_docker.sh
+++ deploy/scripts/start_docker.sh
@@ -5,8 +5,17 @@ aws ecr get-login-password --region eu-north-1 | docker login --username AWS --p
 # Pull the latest image
 docker pull 648485681582.dkr.ecr.eu-north-1.amazonaws.com/emotion_ecr:v3
 
-docker stop my-app || true
-docker rm my-app || true
+# Check if the container 'campusx-app' is running
+if [ "$(docker ps -q -f name=my-app)" ]; then
+    # Stop the running container
+    docker stop my-app
+fi
+
+# Check if the container 'campusx-app' exists (stopped or running)
+if [ "$(docker ps -aq -f name=my-app)" ]; then
+    # Remove the container if it exists
+    docker rm my-app
+fi
 
 # Run a new container
 docker run -d -p 80:5000 -e DAGSHUB_PAT=8682bf42823d1ad87472a46fd14c46cda9ab16cf --name my-app 648485681582.dkr.ecr.eu-north-1.amazonaws.com/emotion_ecr:v3
