910f911bef038537e2da23ff9bb52bbbb453f62f
Saiful0044 <saifulislam00324@gmail.com>
2025-10-24 22:07:14 +0600

    add deployment version 3

--- deploy/scripts/start_docker.sh
+++ deploy/scripts/start_docker.sh
@@ -3,7 +3,7 @@
 aws ecr get-login-password --region eu-north-1 | docker login --username AWS --password-stdin 648485681582.dkr.ecr.eu-north-1.amazonaws.com
 
 # Pull the latest image
-docker pull 648485681582.dkr.ecr.eu-north-1.amazonaws.com/emotion_ecr:v4
+docker pull 648485681582.dkr.ecr.eu-north-1.amazonaws.com/emotion_ecr:v3
 
 # Check if the container 'campusx-app' is running
 if [ "$(docker ps -q -f name=my-app)" ]; then
@@ -18,4 +18,4 @@ if [ "$(docker ps -aq -f name=my-app)" ]; then
 fi
 
 # Run a new container
-docker run -d -p 80:5000 -e DAGSHUB_PAT=8682bf42823d1ad87472a46fd14c46cda9ab16cf --name my-app 648485681582.dkr.ecr.eu-north-1.amazonaws.com/emotion_ecr:v4
+docker run -d -p 80:5000 -e DAGSHUB_PAT=8682bf42823d1ad87472a46fd14c46cda9ab16cf --name my-app 648485681582.dkr.ecr.eu-north-1.amazonaws.com/emotion_ecr:v3
